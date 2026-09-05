93bd288f94af04df196605811e91fabae3ac5962
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2026-09-05 16:01:52 +0100

    test: define M2 theme contracts

--- theme_monynha/tests/test_theme.py
+++ theme_monynha/tests/test_theme.py
@@ -13,13 +13,20 @@ class TestMonynhaTheme(HttpCase):
         website.theme_id = theme
         theme._theme_get_stream_themes().with_context(load_all_views=True)._theme_load(website)
 
-    def test_core_snippets_registered(self):
+    def test_core_and_m2_snippets_registered(self):
         keys = {
             "theme_monynha.s_monynha_hero",
             "theme_monynha.s_monynha_services",
             "theme_monynha.s_monynha_process",
             "theme_monynha.s_monynha_labs",
             "theme_monynha.s_monynha_cta",
+            "theme_monynha.s_monynha_signal",
+            "theme_monynha.s_monynha_selected_work",
+            "theme_monynha.s_monynha_capability",
+            "theme_monynha.s_monynha_manifesto",
+            "theme_monynha.s_monynha_metrics",
+            "theme_monynha.s_monynha_faq",
+            "theme_monynha.s_monynha_intro",
         }
         views = self.env["ir.ui.view"].search([("key", "in", list(keys)), ("website_id", "!=", False)])
         self.assertEqual(set(views.mapped("key")), keys)
